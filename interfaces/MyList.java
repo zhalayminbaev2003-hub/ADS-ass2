839b80b4f6308555d4d867f58fd078ebd786c510
isturganb <zhalayminbaev2003@gmail.com>
2026-04-07 14:06:48 +0500

    feat: add MyList interface

---
+++ interfaces/MyList.java
@@ -0,0 +1,26 @@
+import java.util.Iterator;
+
+/**
+ * This interface defines the basic operations for our custom list structures.
+ * It extends Iterable so we can use "for-each" loops.
+ */
+public interface MyList<T> extends Iterable<T> {
+    void add(T item);
+    void set(int index, T item);
+    void add(int index, T item);
+    void addFirst(T item);
+    void addLast(T item);
+    T get(int index);
+    T getFirst();
+    T getLast();
+    void remove(int index);
+    void removeFirst();
+    void removeLast();
+    void sort();
+    int indexOf(Object object);
+    int lastIndexOf(Object object);
+    boolean exists(Object object);
+    Object[] toArray();
+    void clear();
+    int size();
+}
